121e98658d07a009b351d80ec06de05bb11b309d
jaz <70194414+Jaz123-rgb@users.noreply.github.com>
2022-12-09 17:43:17 -0600

    .

---
+++ SelectionSort.py
@@ -0,0 +1,20 @@
+def findSmalest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+
+
+def SelectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = findSmalest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+
+print (SelectionSort([5,3,6,2,10]))
